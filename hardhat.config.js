2a905a6078ebf8241714aa85d1ac52394723c43c
Jumcee <futhmah456@gmail.com>
2024-11-05 16:53:07 +0100

    crosschain

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,7 @@ const { ARBITRUM_URL, PRIVATE_KEY } = process.env
 module.exports = {
   solidity: "0.8.1",
   networks: {
-    arbitrumSepolia: {
+    UnichainSepoliaTestnet: {
       url: process.env.ARBITRUM_URL,
       chainId: 421614,
       accounts: [process.env.PRIVATE_KEY],
