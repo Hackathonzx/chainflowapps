a95751feded8458e07b066f958c7bc8c739040f1
Jumcee <futhmah456@gmail.com>
2024-11-06 10:25:07 +0100

    liquidity

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,17 +2,15 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ethers");
 require("dotenv").config();
 
-// require("hardhat-arbitrum");
-
-const { ARBITRUM_URL, PRIVATE_KEY } = process.env
+const { UNICHAIN_URL, PRIVATE_KEY } = process.env
 
 
 module.exports = {
   solidity: "0.8.1",
   networks: {
     UnichainSepoliaTestnet: {
-      url: process.env.ARBITRUM_URL,
-      chainId: 421614,
+      url: process.env.UNICHAIN_URL,
+      chainId: 1301,
       accounts: [process.env.PRIVATE_KEY],
    },
  },
--- ignition/modules/deploy.js
+++ ignition/modules/deploy.js
@@ -6,7 +6,7 @@ async function main() {
 
     // Define the addresses for Chainlink CCIP Router and Interest Rate Oracle
     const chainlinkCCIPRouterAddress = "0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59"; // Replace with actual address
-    const interestRateOracleAddress = "0xYourInterestRateOracleAddress"; // Replace with actual address
+    const interestRateOracleAddress = "0x2880aB155794e7179c9eE2e38200202908C17B43"; //
 
     // Deploy the contract
     const aggregator = await CrossChainLiquidityAggregator.deploy(chainlinkCCIPRouterAddress, interestRateOracleAddress);
@@ -14,7 +14,7 @@ async function main() {
     // Wait for the deployment to complete
     await aggregator.waitForDeployment();
 
-    console.log("CrossChainLiquidityAggregator deployed to:", aggregator.address);
+    console.log("CrossChainLiquidityAggregator deployed to:", await aggregator.getAddress());
 }
 
 // Run the deployment script
